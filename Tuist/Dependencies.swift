f72928a6d6bb019d7f8c0127d217835bbbd4230f
Eric Gimenez Galera <eric.gimenez.galera@gmail.com>
2023-07-07 08:42:16 +0200

    chore: change OHHTTPStubs dependency from carthage to SPM
    This change is provoke by this issue https://github.com/AliSoftware/OHHTTPStubs/issues/358

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -8,10 +8,8 @@
 import ProjectDescription
 
 let dependencies = Dependencies(
-    carthage: [
-        .github(path: "AliSoftware/OHHTTPStubs", requirement: .upToNext("9.0.0")),
-    ],
     swiftPackageManager: [
+        .remote(url: "https://github.com/AliSoftware/OHHTTPStubs", requirement: .upToNextMajor(from: "9.0.0")),
         .remote(url: "https://github.com/Alamofire/Alamofire", requirement: .upToNextMajor(from: "5.0.0")),
     ],
     platforms: [.iOS]
